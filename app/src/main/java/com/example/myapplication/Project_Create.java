994699e9cf493fef71d6cb35c488179eb2163707
omkpo <om.k.poojary@gmail.com>
2023-06-12 00:31:16 +0530

    New updates happening in firestore

--- app/src/main/java/com/example/myapplication/Project_Create.java
+++ app/src/main/java/com/example/myapplication/Project_Create.java
@@ -15,18 +15,24 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Project_Create extends AppCompatActivity {
-    String memebers[] = {};
-
+    //String memebers[] = {};
+    List<String> memebers = new ArrayList<>();
     ListView memeberList;
     Button createProj,addUse;
-    EditText maembName;
+    EditText maembName,projName;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -38,16 +44,37 @@ public class Project_Create extends AppCompatActivity {
         createProj = findViewById(R.id.create);
         addUse = findViewById(R.id.addUser);
         maembName = findViewById(R.id.mamberName);
+        projName = findViewById(R.id.pName);
+
         memeberList = findViewById(R.id.ListM);
 
         createProj.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Map<String, Object> user2 = new HashMap<>();
+                user2.put("Memebers", memebers);
 
 
-                Intent intent = new Intent(Project_Create.this,fragment1.class);
-                intent.putExtra("key", memebers);
-                startActivity(intent);
+                CollectionReference collectionRef = db.collection(Log_In.userName);
+                DocumentReference documentRef = collectionRef.document(projName.getText().toString());
+                //Map<String, Object> documentData = new HashMap<>();
+                //documentData.put("arrayField", memeberList);
+                documentRef.set(user2)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Project_Create.this, "Added", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Project_Create.this,HomePage.class);
+                                    //intent.putExtra("key", memebers);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(Project_Create.this, "No added", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
             }
         });
 
@@ -62,8 +89,9 @@ public class Project_Create extends AppCompatActivity {
                             QuerySnapshot document = task.getResult();
                             if (!document.isEmpty()) {
                                 // Collection exists
-                                memebers = Arrays.copyOf(memebers, memebers.length + 1);
-                                memebers[memebers.length - 1] = maembName.getText().toString();
+                                //memebers = Arrays.copyOf(memebers, memebers.length + 1);
+                                //memebers[memebers.length - 1] = maembName.getText().toString();
+                                memebers.add(maembName.getText().toString());
                                 for(String i:memebers)
                                 {
                                     System.out.println(i);
